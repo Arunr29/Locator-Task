b2cd810bcb43332ecc1e74418c1cef1f64c86963
Suresh-Antony~ <Antony@makolet.in>
2023-04-18 20:49:22 +0530

    added navigations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
+// import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
@@ -18,11 +24,11 @@ import { HttpClientModule} from '@angular/common/http'
     NgxSpinnerModule,
     ReactiveFormsModule,
     FormsModule,
-    ToastrModule,
-    ToastrModule.forRoot({
-      preventDuplicates: true,
-      timeOut: 15000
-    }),
+    // ToastrModule,
+    // ToastrModule.forRoot({
+    //   preventDuplicates: true,
+    //   timeOut: 15000
+    // }),
     BrowserAnimationsModule,
     HttpClientModule,
   ],
